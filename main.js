28db0198a15748383b46399e1179e897e73721ac
Gabbo-CYBER <gabry29101998@gmail.com>
2024-04-22 11:20:16 +0200

    end

---
+++ main.js
@@ -0,0 +1,15 @@
+let i = 1
+const contenitore = document.getElementById("container_numbers")
+for (let i = 1; i <= 100; i++) {
+    console.log("")
+
+
+    if (i % 5 == 0){
+        console.log("buzz")
+    }
+
+    if (i % 3 == 0){
+        console.log("fizz")
+    }
+
+}
